634dec2176deb295f1df8e90d769e8098b132409
JonathanJouffroy <jonathanjouffroy@gmail.com>
2020-04-03 09:57:15 +0200

    atlas 3

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.use((req,res,next) => {
     res.header('Access-Control-Allow-Headers','Origin, X-Requested-With, Content-Type, Accept')
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
     next()
-})
+}) 
 
 
 /*mongoose.Promise = Promise
